6fc08c765fde6cff94ee51c5415112109bc9a6f6
toshif <toshif@com>
2012-07-29 01:06:34 +0900

    added some more samples of qunit assertions

--- js-samples/src/qunit-tests.js
+++ js-samples/src/qunit-tests.js
@@ -1,7 +1,19 @@
+// reference : http://api.qunitjs.com/
+
+// test( title: String[, expected: Number ], test: Function(assert: QUnit.assert) )
+
+// ok : A boolean assertion, equivalent to CommonJSÅfs assert.ok() and JUnitÅfs assertTrue(). Passes if the first argument is truthy.
+
+// equal : A comparison assertion, equivalent to JUnit/CommonJS assertEqual(s).
+
 test("hello test", function() {
 	ok(1 == "1", "Passed nice!");
 });
 
-test("hello test2", function() {
-	ok(1 == "1", "Passed nice 2!");
+test("hello test equal 2", function() {
+	equal(1, "1");
+	equal(1, "1", "good matched");
+	equal(2, "2", "good matched twice");
+	equal("aaa", "aaa");
 });
+

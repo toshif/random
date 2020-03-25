3de54ad55a19fb45913d00e82cfc4e3fa14eefe1
toshif <tfukuzawa@live.jp>
2020-03-25 17:03:07 +0000

    abc

--- radiko-links/index.js
+++ radiko-links/index.js
@@ -1,4 +1,4 @@
-var MY_FAVORITES = [
+const MY_FAVORITES = [
     { 'station': 'FMT', 'hour': '0000', 'name': '---- Tokyo FM ----' },
     { 'station': 'FMT', 'hour': '0600', 'name': 'One Morning' },
     { 'station': 'FMT', 'hour': '0900', 'name': 'Blue Ocean' },
@@ -13,15 +13,23 @@ var MY_FAVORITES = [
     { 'station': 'FMJ', 'hour': '1500', 'name': 'Good neighbors 2' },
 ];
 
+const wdays = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+
 $(document).ready(function () {
     var links = "";
-    MY_FAVORITES.forEach(function (item, index) {
-        var today = new Date();
-        var tmonth = ("0" + (today.getMonth() + 1)).slice(-2);
-        var tdate = ("0" + today.getDate()).slice(-2);
-        var today_str = '' + today.getUTCFullYear() + tmonth + tdate;
-        href = 'http://radiko.jp/#!/ts/' + item['station'] + '/' + today_str + item['hour'] + '00';
-        links += '<li><a href="' + href + '">' + item['station'] + ' ' + item['hour'] + ' ' + item['name'] + '</a></li>';
+    var yesterday = new Date();
+    yesterday.setDate(yesterday.getDate() - 1);
+    var dates = [new Date(), yesterday];
+    dates.forEach(function (target_date, idx1) {
+        links += '<li>' + wdays[target_date.getDay()] + ' ' + target_date.getUTCDate() + '</li>'
+        MY_FAVORITES.forEach(function (item, index) {
+            var today = target_date;
+            var tmonth = ("0" + (today.getMonth() + 1)).slice(-2);
+            var tdate = ("0" + today.getDate()).slice(-2);
+            var today_str = '' + today.getUTCFullYear() + tmonth + tdate;
+            href = 'http://radiko.jp/#!/ts/' + item['station'] + '/' + today_str + item['hour'] + '00';
+            links += '<li><a href="' + href + '">' + item['station'] + ' ' + item['hour'] + ' ' + item['name'] + '</a></li>';
+        });
     });
     $("#my-links").html(links);
 });
